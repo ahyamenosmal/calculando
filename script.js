920f2a1f24375e6ea625010a9bd041f01bc75db6
Evelyn Flores <evelyn.flores.caa@gmail.com>
2024-06-02 15:58:34 -0400

    my third commit

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ sumar.addEventListener("click", ()=>{
 let aumentar = document.querySelector(".cantidad")
 let valor = aumentar.innerHTML
 aumentar.innerHTML = Number(valor) + 1
-let pagar = document.querySelector(".valor-total")    
-pagar.innerHTML = Number(aumentar.innerHTML) * precio
+actualizarValorTotal()
+
 })
 
 
 restar.addEventListener("click", ()=>{
 let disminuir = document.querySelector(".cantidad")
 let valor = disminuir.innerHTML
-disminuir.innerHTML = Number(valor) - 1
-let pagar = document.querySelector(".valor-total")    
-pagar.innerHTML = Number(disminuir.innerHTML) * precio    
+if(Number(valor) > 0){
+    disminuir.innerHTML = Number(valor) - 1
+    actualizarValorTotal()
+
+}
+    
 })
+
+function actualizarValorTotal() {
+    const cantidad = document.querySelector(".cantidad").innerHTML;
+    const pagar = document.querySelector(".valor-total");
+    pagar.innerHTML = Number(cantidad) * precio;
+  }
+
+
+
